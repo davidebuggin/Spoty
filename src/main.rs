b645b0af474f28b51e2c2c3e9b46626f4e165a6b
davidebuggin <davide.buggin@gmail.com>
2024-04-18 15:20:04 +0200

    changed the artist

--- src/main.rs
+++ src/main.rs
@@ -59,7 +59,7 @@ fn print_tracks(tracks: Vec<&Track>) {
 async fn main() {
     let url = format!(
         "https://api.spotify.com/v1/search?q={query}&type=track,artist",
-        query = "Lazza"
+        query = "emis killa"
     );
     let client = reqwest::Client::new();
     let response = client
