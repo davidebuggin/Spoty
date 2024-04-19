0285dd6145b4144559788f74680c2159a1237fb9
davidebuggin <davide.buggin@gmail.com>
2024-04-19 11:20:11 +0200

    create Cli struct and add clap library

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ use reqwest::{
 
 use serde::{Deserialize, Serialize};
 
+use clap::Parser;
+
 #[derive(Serialize, Deserialize, Debug)]
 struct ExternalUrls {
     spotify: String,
@@ -15,6 +17,7 @@ struct Artist {
     external_urls: ExternalUrls,
 }
 #[derive(Serialize, Deserialize, Debug)]
+
 struct Album {
     name: String,
     artists: Vec<Artist>,
@@ -36,35 +39,27 @@ struct Items<T> {
 struct APIResponse {
     tracks: Items<Track>,
 }
-
-fn print_tracks(tracks: Vec<&Track>) {
-    for track in tracks {
-        println!("🔥 {}", track.name);
-        println!("💿 {}", track.album.name);
-        println!(
-            "🕺 {}",
-            track
-                .album
-                .artists
-                .iter()
-                .map(|artist| artist.name.to_string())
-                .collect::<String>()
-        );
-        println!("🌎 {}", track.external_urls.spotify);
-        println!("---------")
-    }
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[arg(short, long, value_name = "TOKEN")]
+    token : String,
+    #[arg(short, long, value_name = "ARTIST")]
+    artist : String,
 }
 
 #[tokio::main]
 async fn main() {
+    let cli = Cli::parse();
+
     let url = format!(
         "https://api.spotify.com/v1/search?q={query}&type=track,artist",
-        query = "emis killa"
+        query = cli.artist
     );
     let client = reqwest::Client::new();
     let response = client
         .get(url)
-        .header(AUTHORIZATION, "Bearer BQCgHPIq39tgYWWaJag21nybX5___1FbCl1_o7CzjCkBowHMKiFfxhWQgrxQBmQZaN_cNaZLhf1RXE0dsH88E5X80qyIPC6pr_HuFYE_gRz0khBPkd8")
+        .header(AUTHORIZATION, format!("Bearer {}" , cli.token))
         .header(CONTENT_TYPE, "application/json")
         .header(ACCEPT, "application/json")
         .send()
@@ -84,4 +79,29 @@ async fn main() {
             panic!("Something unexpected happened : {:?}", other);
         }
     };
+}
+
+
+
+
+
+
+
+
+fn print_tracks(tracks: Vec<&Track>) {
+    for track in tracks {
+        println!("🔥 {}", track.name);
+        println!("💿 {}", track.album.name);
+        println!(
+            "🕺 {}",
+            track
+                .album
+                .artists
+                .iter()
+                .map(|artist| artist.name.to_string())
+                .collect::<String>()
+        );
+        println!("🌎 {}", track.external_urls.spotify);
+        println!("---------")
+    }
 }
