38a8d3cdfc84fb6e303fd9ccba96ee57c3d265b4
davidebuggin <davide.buggin@gmail.com>
2024-04-22 17:07:26 +0200

    Resolved the mismatched types problem at 84

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use std::error::Error;
 
+use clap::Parser;
 use reqwest::{
     self,
-    header::{ACCEPT, AUTHORIZATION, CONTENT_TYPE}, Client,
+    header::{ACCEPT, AUTHORIZATION, CONTENT_TYPE},
+    Client,
 };
 use serde::{Deserialize, Serialize};
-use clap::Parser;
 #[derive(Serialize, Deserialize, Debug)]
 struct ExternalUrls {
     spotify: String,
@@ -31,7 +32,7 @@ struct Track {
     external_urls: ExternalUrls,
 }
 #[derive(Serialize, Deserialize, Debug)]
-struct Items{
+struct Items {
     items: Vec<Track>,
 }
 #[derive(Serialize, Deserialize, Debug)]
@@ -51,7 +52,7 @@ fn tracks_into_string(tracks: Vec<Track>) -> String {
 
     for track in tracks {
         result.push_str(&format!("🎶 TITLE: {} \n", track.name));
-        result.push_str(&format!("💿 ALBUM: {} \n" , track.album.name));
+        result.push_str(&format!("💿 ALBUM: {} \n", track.album.name));
         result.push_str(&format!(
             "🕺 ARTIST: {} \n",
             track
@@ -73,15 +74,15 @@ fn tracks_into_string(tracks: Vec<Track>) -> String {
 async fn main() {
     let cli = Cli::parse();
 
-    let spotify_client = SpotifyClient{
-        token : cli.token,
-        client : reqwest::Client::new(), 
+    let spotify_client = SpotifyClient {
+        token: cli.token,
+        client: reqwest::Client::new(),
     };
 
-    match spotify_client.get_tracks(&cli.artist).await{
+    match spotify_client.get_tracks(&cli.artist).await {
         Ok(tracks) => {
-            let track_string = tracks_into_string(tracks);
-            println!("{}" , track_string);
+            let track_string = tracks_into_string(tracks.items);
+            println!("{}", track_string);
         }
         Err(err) => {
             println!("{}", err);
@@ -89,28 +90,29 @@ async fn main() {
     }
 }
 
-trait ClientSpotify{
-    async fn get_tracks(&self, artist : &str) -> Result<Items, Box<dyn Error>>;
+trait ClientSpotify {
+    async fn get_tracks(&self, artist: &str) -> Result<Items, Box<dyn Error>>;
 }
 
 struct SpotifyClient {
-    token : String,
-    client : Client,
+    token: String,
+    client: Client,
 }
 
 impl ClientSpotify for SpotifyClient {
-    async fn get_tracks(&self, artist : &str) -> Result<Items, Box<dyn Error>> {
+    async fn get_tracks(&self, artist: &str) -> Result<Items, Box<dyn Error>> {
         let url = format!(
             "https://api.spotify.com/v1/search?q={query}&type=track,artist",
             query = artist
         );
-        let response = self.client
-        .get(url)
-        .header(AUTHORIZATION, format!("Bearer {}", self.token))
-        .header(CONTENT_TYPE, "application/json")
-        .header(ACCEPT, "application/json")
-        .send()
-        .await?;
+        let response = self
+            .client
+            .get(url)
+            .header(AUTHORIZATION, format!("Bearer {}", self.token))
+            .header(CONTENT_TYPE, "application/json")
+            .header(ACCEPT, "application/json")
+            .send()
+            .await?;
 
         match response.status() {
             reqwest::StatusCode::OK => {
@@ -128,30 +130,30 @@ impl ClientSpotify for SpotifyClient {
     }
 }
 
-
 #[cfg(test)]
 mod test {
     use super::*;
     #[test]
     fn test_tracks() {
         let test_track = Track {
-            name : "Song".to_string(),
-            album : Album {
-                name : "Album".to_string(),
-                artists : vec![
-                    Artist{
-                        name : "Artist 1".to_string(),
-                        external_urls : ExternalUrls {
+            name: "Song".to_string(),
+            album: Album {
+                name: "Album".to_string(),
+                artists: vec![
+                    Artist {
+                        name: "Artist 1".to_string(),
+                        external_urls: ExternalUrls {
                             spotify: "http://example.com".to_string(),
-                        }
+                        },
                     },
-                    Artist{
-                        name : "Artist 2".to_string(),
-                        external_urls : ExternalUrls {
+                    Artist {
+                        name: "Artist 2".to_string(),
+                        external_urls: ExternalUrls {
                             spotify: "http://example.com".to_string(),
-                        }
-                    }],
-                external_urls : ExternalUrls {
+                        },
+                    },
+                ],
+                external_urls: ExternalUrls {
                     spotify: "http://example.com".to_string(),
                 },
             },
@@ -166,9 +168,8 @@ mod test {
 
         let expected_result = "🎶 TITLE: Song \n💿 ALBUM: Album \n🕺 ARTIST: Artist 1, Artist 2 \n🌎 LINK: http://example.com \n------------------------------------------------------------------------------------------------------- \n";
 
-        assert_eq!(result , expected_result);
+        assert_eq!(result, expected_result);
     }
 }
-
 
 //cargo run -- --token <token> --artist "<nomeartista>"
